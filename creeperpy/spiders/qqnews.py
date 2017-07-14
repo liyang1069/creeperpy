d4a675f095ab60104b9d95ac26d42a37a1e1014c
liyang <18678395180@163.com>
2017-07-14 10:36:24 +0800

    last

--- creeperpy/spiders/qqnews.py
+++ creeperpy/spiders/qqnews.py
@@ -4,10 +4,16 @@ sys.path.append("../..")
 import time
 import scrapy
 import hashlib
+import json
 from creeperpy.items import CreeperpyItem
 from scrapy.http import HtmlResponse
 from scrapy.http import Request
 from hbase_handler import HbaseHandler
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+topic = "creeper"
 
 class QQCreeper(scrapy.Spider):
   """docstring for QQCreeper"""
@@ -18,6 +24,10 @@ class QQCreeper(scrapy.Spider):
   allowed_domains = ["qq.com"]
   start_urls = ["http://news.qq.com/"]
 
+
+  # Asynchronous by default
+  # future = producer.send('creeper', b'raw_bytes')
+
   def parse_item(self, response):
     item = response.meta['item']
     news_list = response.xpath('//div[@id="Cnt-Main-Article-QQ"]').extract()
@@ -25,19 +35,22 @@ class QQCreeper(scrapy.Spider):
       return
     content = news_list[0]
     item['content'] = content.replace("\r\n","").replace("\n","")
+    global producer, topic
     # file = open("out_file/qq.txt","ab")
     # try:
     #   file.write(("\t".join([item['time_str'], item['url'], item['title'], item['content']]) + "\n").encode('utf-8'))
     # finally:
     #   file.close()
-    hbase = HbaseHandler()
-    try:
-      md = hashlib.md5()
-      md.update(item['url'].encode('utf-8'))
-      if b'cf1:url' not in hbase.row("origin_news", md.hexdigest()):
-        hbase.put("origin_news", md.hexdigest(), {"cf1:url": item['url'], "cf1:title": item['title'], "cf1:content": item['content']})
-    finally:
-      hbase.close()
+    # hbase = HbaseHandler()
+    # try:
+    #   md = hashlib.md5()
+    #   md.update(item['url'].encode('utf-8'))
+    #   if b'cf1:url' not in hbase.row("origin_news", md.hexdigest()):
+    #     hbase.put("origin_news", md.hexdigest(), {"cf1:url": item['url'], "cf1:title": item['title'], "cf1:content": item['content']})
+    # finally:
+    #   hbase.close()
+    producer.send(topic, json.dumps({"cf1:url": item['url'], "cf1:title": item['title'], "cf1:content": item['content']}).encode())
+
 
   def parse(self, response):
     for line_a in response.xpath('//div[@class="item major"]//a[@class="linkto"]'):
@@ -46,4 +59,7 @@ class QQCreeper(scrapy.Spider):
       item['url'] = line_a.xpath('@href').extract()[0]
       item['title'] = line_a.xpath('text()').extract()[0]
       yield Request(item['url'], meta={'item': item}, callback=self.parse_item)
+
+    global producer, topic
+    producer.flush()
     
